5937f08aa7764d3808e0ce96c601d2e7f81cf6da
taeyoungNew <dndbxhd10@naver.com>
2023-08-08 01:24:10 +0900

    [수정] 삭제 순서변경

--- routes/columns.route.js
+++ routes/columns.route.js
@@ -88,17 +88,17 @@ router.put('/:board_id/column_order/:column_id', authMiddleware, async (req, res
   const { order } = req.body;
   // 변경하고 싶은 순서
   try {
-    // 컬럼의 정보를 가져오기
+    // 해당 컬럼의 정보를 가져오기
     const columnInfo = await Columns.findOne({ where: { columnId: column_id, boardId: board_id } });
     if (!columnInfo) return res.status(412).json({ message: '해당 컬럼을 찾지 못했습니다.' });
 
     // 현재의 순번보다 변경하려는 순번값이 높은지 낮은지 먼저 확인한다.
     if (columnInfo.order > order) {
       // 순번을 앞으로 할때
-      // 순번을 바꾸려는 컬럼보다 낮은 순번의 컬럼의 모든 정보를 가져오기
+      // 변경순번이상 현재순번 미만의 컬럼의 모든 정보를 가져오기
       const columns = await Columns.findAll({
-        // Op.lt = 미만
-        where: { boardId: board_id, order: { [Op.lt]: columnInfo.order } },
+        // Op.gte = 이상 Op.lt = 미만
+        where: { boardId: board_id, order: { [Op.gte]: order, [Op.lt]: columnInfo.order } },
       });
       for (let idx = 0; idx < columns.length; idx++) {
         // 근데 만약 바로 다음의 숫자가 적힌 order가 아니면 거기서 return
@@ -109,9 +109,9 @@ router.put('/:board_id/column_order/:column_id', authMiddleware, async (req, res
           { transaction: t },
         );
       }
-    } else {
+    } else if (columnInfo.order < order) {
       // 순번을 뒤로 할때
-      // 바꾸려는 순번보다 낮은 순번의 컬럼들을 불러온다
+      // 바꾸려는 순번이하 현재의 순번초과의 컬럼들을 불러온다
       const columns = await Columns.findAll({
         // Op.lt = 미만
         where: { boardId: board_id, order: { [Op.gt]: columnInfo.order, [Op.lte]: order } },
@@ -125,6 +125,8 @@ router.put('/:board_id/column_order/:column_id', authMiddleware, async (req, res
           { transaction: t },
         );
       }
+    } else if (columnInfo.order === order) {
+      return res.status(412).json({ message: '현재 순번입니다.' });
     }
     // 해당 컬럼보다 순번이 빨랐던 컬럼들이 1씩 밀리면
     // 해당 컬럼의 order를 수정
@@ -140,26 +142,47 @@ router.put('/:board_id/column_order/:column_id', authMiddleware, async (req, res
 
 // 컬럼 삭제
 router.delete('/:board_id/column/:column_id', authMiddleware, async (req, res) => {
+  const t = await sequelize.transaction({
+    islationLeval: Transaction.ISOLATION_LEVELS.READ_COMMITTED, // 격리수준을 READ_COMMITTED로 설정
+  });
   const user = res.locals.user;
   const { board_id, column_id } = req.params;
   try {
     // 컬럼의 정보를 가져오기
     const columnInfo = await Columns.findOne({ where: { columnId: column_id } });
     if (!columnInfo) return res.status(412).json({ message: '해당 컬럼을 찾지 못했습니다.' });
-    // 유저보드에 있는 유저의 정보를 가져오기 (보드유저테이블이 없으므로 주석)
+    // 유저보드에 있는 유저의 정보를 가져오기
     const userBoard = await UserBoards.findOne({
       where: { userId: user.userId, boardId: board_id },
     });
-    // 가져온 유저가 관리자인지 확인한다. (보드유저테이블이 없으므로 주석)
+    // 가져온 유저가 관리자인지 확인한다.
     if (userBoard.isAdmin === false)
       return res.status(403).json({ message: '삭제권한이 없습니다.' });
 
-    await Columns.destroy({
-      where: { columnId: column_id, boardId: board_id },
+    // 삭제하려는 컬럼의 순번이 초과인 컬럼들을 가져오기
+    const columns = await Columns.findAll({
+      // Op.gte = 이상 Op.lt = 미만
+      where: { boardId: board_id, order: { [Op.gt]: columnInfo.order } },
     });
-
+    for (let idx = 0; idx < columns.length; idx++) {
+      // 근데 만약 바로 다음의 숫자가 적힌 order가 아니면 거기서 return
+      // 바꾸려는 컬럼보다 순번이 빠른 컬럼들의 order에 1씩 추가 하여 order를 업데이트
+      await Columns.update(
+        { order: columns[idx].order - 1 },
+        { where: { columnId: columns[idx].columnId } },
+        { transaction: t },
+      );
+    }
+    await Columns.destroy(
+      {
+        where: { columnId: column_id, boardId: board_id },
+      },
+      { transaction: t },
+    );
+    await t.commit();
     return res.status(200).json({ message: '해당 컬럼이 삭제되었습니다.' });
   } catch (error) {
+    await t.rollback();
     return res.status(400).json({ message: error });
   }
 });
